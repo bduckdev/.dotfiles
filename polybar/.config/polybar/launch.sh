bc3295605f3317f00035ec028de467879e3fd226
Brennan Duck <brennantduck@gmail.com>
2024-02-21 09:30:27 -0500

    added numerous things and made my computer startup correctly

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,8 +7,8 @@ while pgrep  polybar >/dev/null; do sleep 1; done
 polybar primary &
 
 if xrandr | grep 'DP-1-2 connected' ; then
-    polybar secondary &
     polybar fakenews &
+    polybar secondary &
 fi
 
 nitrogen --restore
