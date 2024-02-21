9b8adad1f2f104de7392246a63b85424995a3212
Brennan Duck <brennantduck@gmail.com>
2024-02-21 09:32:18 -0500

    added wezterm

---
+++ wezterm/.config/wezterm/lua/background.lua
@@ -0,0 +1,14 @@
+Background = {
+	{
+		source = {
+			File = "/home/bduck/Pictures/Wallpapers/windows_vista.jpg",
+		},
+		width = "100%",
+		repeat_x = "NoRepeat",
+		repeat_y_size = "200%",
+		hsb = {
+			saturation = 1,
+			brightness = 0.35,
+		},
+	},
+}
